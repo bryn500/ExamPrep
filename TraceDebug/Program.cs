fda9c40ef1a73efe98b0458cbe610ac74af18b81
Bryn Thomas <bryn.thomas@education.gov.uk>
2020-10-27 17:17:51 +0000

    Conditional attribute examples added to Debug

--- TraceDebug/Program.cs
+++ TraceDebug/Program.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//#define CONDITION1
+#define CONDITION2
+#define CONDITION3
+using System;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TraceDebug
 {
@@ -16,7 +15,7 @@ namespace TraceDebug
         {
             Trace.TraceInformation("Something happened");
             Trace.TraceError("Oh no! Something happened");
-            
+
             ts.TraceInformation("Hello"); // won't be printed
             ts.TraceEvent(TraceEventType.Error, 0, "!!! Hello !!!");
             ts.Switch.Level = SourceLevels.Verbose; // changing logging level
@@ -26,6 +25,35 @@ namespace TraceDebug
             ts.Close();
 
             Console.ReadLine();
+
+
+            Console.WriteLine("Calling Method1");
+            Method1(3);
+            Console.WriteLine("Calling Method2");
+            Method2();
+#if CONDITION3
+            Console.WriteLine("Condition3 is defined");
+#endif
         }
+
+        [Conditional("CONDITION1")]
+        public static void Method1(int x)
+        {
+            Console.WriteLine("CONDITION1 is defined");
+        }
+
+        [Conditional("CONDITION1"), Conditional("CONDITION2")]
+        public static void Method2()
+        {
+            Console.WriteLine("CONDITION1 or CONDITION2 is defined");
+        }
+
+        // must be void
+        //[Conditional("CONDITION2")]
+        //public static int Method3(int x)
+        //{
+        //    Console.WriteLine("CONDITION2 is defined");
+        //    return 1;
+        //}
     }
 }
